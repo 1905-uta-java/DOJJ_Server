fde754800a2e699457334c5466e95484dd9fc7a3
MartinD4574 <dcm9595@gmail.com>
2019-07-08 14:32:40 -0500

    Fix Database

--- src/main/java/com/moviereviews/models/Review.java
+++ src/main/java/com/moviereviews/models/Review.java
@@ -7,10 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name="REVIEWS")
@@ -24,9 +21,7 @@ public class Review implements Serializable{
 	@Column(name="REVIEW_ID", nullable=false)
 	private int id;
 	
-	@Transient
-	@ManyToOne
-	@JoinColumn(name="username", nullable=false)
+	@Column(name="USER_NAME")
 	private String username;
 	@Column(name="REVIEW_CONTENT")
 	private String reviewContent;
